53af6d49b89300b692ab05a47bffa3febea20287
marcko <marco.itnl@gmail.com>
2014-04-17 12:26:16 -0500

    se crean tablas de proyecto, se realiza instalacion de south , migracion de modelo track a south, se crea una vista llamada track.html

--- Sfotipy/albums/models.py
+++ Sfotipy/albums/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+from  artists.models import Artist
+
+class Album(models.Model):
+	title = models.CharField(max_length=255)
+	cover = models.ImageField(upload_to='cover_file')
+	artist = models.ForeignKey(Artist)
+
+	def __str__(self):
+		return self.title
--- Sfotipy/tracks/models.py
+++ Sfotipy/tracks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+from albums.models import Album 
+from artists.models import Artist 
+
+
+class Track(models.Model):
+	title = models.CharField(max_length=255)
+	order = models.PositiveIntegerField()
+	track = models.FileField(upload_to= 'tracks_file')
+	artist = models.ForeignKey(Artist)
+	album = models.ForeignKey(Album)
+
+	def __str__(self):
+		return self.title
